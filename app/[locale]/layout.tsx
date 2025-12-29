9aa5ee37d617d1e47e6ec2368a8925f218e4bdb7
HASSANBOB533 <244470639+HASSANBOB533@users.noreply.github.com>
2025-12-29 08:25:50 -0500

    Add professional favicon from Best of Bedz logo in multiple sizes

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -60,7 +60,10 @@ export default async function LocaleLayout({
         
         {/* Favicon and Icons */}
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#2F63AD" />
