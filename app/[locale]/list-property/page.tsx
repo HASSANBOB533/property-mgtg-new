c27c778cb9c66df02823b88e3bd842d7a9aa7971
HASSANBOB533 <244470639+HASSANBOB533@users.noreply.github.com>
2025-12-29 08:50:59 -0500

    Remove all extra padding and margins to eliminate white space before footer

--- app/[locale]/list-property/page.tsx
+++ app/[locale]/list-property/page.tsx
@@ -2,8 +2,8 @@ import PropertyForm from '@/components/PropertyForm';
 
 export default function ListPropertyPage() {
   return (
-    <div className="py-6 md:py-8">
+    <>
       <PropertyForm />
-    </div>
+    </>
   );
 }
